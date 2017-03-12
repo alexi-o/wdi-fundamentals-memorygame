99505d45151d4b3dee86be2c1d523da06a1ee79f
AlexiOHearn <alexiohearn@o.local>
2017-03-12 16:38:39 -0600

    Unit 11 Assignment

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,7 +25,19 @@ var cards = [
 
 var cardsInPlay = [];
 
-var flipCard = function(cardId) {
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2){
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again...");
+		}
+	}
+};
+
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	this.src = cards[cardId].cardImage;
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].suit);
@@ -33,18 +45,18 @@ var flipCard = function(cardId) {
 	checkForMatch();
 }
 
-var checkForMatch = function() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again...");
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+			var cardElement = document.createElement('img');
+			cardElement.setAttribute('src', "images/back.png");
+			cardElement.setAttribute('data-id', i);
+			cardElement.addEventListener('click', flipCard);
+			document.getElementById('game-board').appendChild(cardElement);
+	}
 }
-}
 
-
-flipCard(0);
-flipCard(2);
-
+createBoard();
 
 
 
